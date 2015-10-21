61269f2496ebb810e9d9e0d45366e11dd7eb66ae
Luiz G. Ribeiro <ltrindaderibeiro@gmail.com>
2015-10-21 19:51:17 -0200

    Alocacao de espaco

--- trabalho1.c
+++ trabalho1.c
@@ -5,4 +5,29 @@
  */
 
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
+void main (){
+
+        //dicionario de dados
+        char *wordList;
+
+        wordList = (char *) calloc (10, sizeof(char));
+
+        if (wordList==NULL){
+                printf("Erro de alocacao\n");
+        } else{ 
+                printf("Tudo certo\n");
+        }
+
+        //colocando conteudo para teste 
+        
+        scanf("%s", wordList);
+        printf("%s\n", wordList);
+        if(wordList[2]=='\0'){
+                printf("agora sim sabemos como separam\n");
+        }
+        free(wordList);        
+        return;
+}
