585122b18c75eebafd0ee824577223f8a2210090
Luiz G. Ribeiro <ltrindaderibeiro@gmail.com>
2015-10-24 19:15:04 -0200

    pegando char a char

--- trabalho1.c
+++ trabalho1.c
@@ -12,7 +12,7 @@ void main (){
 
         //dicionario de dados
         char *wordList;
-        int letterCount, pointerLen;
+        int i, letterCount=0, pointerLen, j=0;
 
 
         //alocando espaço na memoria
@@ -25,6 +25,26 @@ void main (){
                 printf("Tudo certo\n");
         }
        
+        //teste de retorno do scanf
+        //--> pegar palavras digitadas 
+        //--> contar quantas letras
+        //-->realocar espaço
+        
+
+       // for (i=0; i>=0, i++){
+                while(1){ 
+                        scanf("%c", wordList+letterCount);
+                        printf("%c", wordList[letterCount]);
+                        letterCount++;
+                        j++;
+                }
+                //adicionado por conta do \0 no final das palavras
+                letterCount++;
+                if(letterCount>=(pointerLen/2)){
+                          wordList=(char *) realloc(wordList,pointerLen*2);
+                          printf("Entrou aqui\n");
+                }
+        printf("%d\n\n", letterCount);
         
        scanf("%s %s", wordList, wordList+4);
        letterCount=strlen(wordList);
