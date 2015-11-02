f98a3f6fe336e76ffa24ff0373f328e9d444974f
Luiz G. Ribeiro <ltrindaderibeiro@gmail.com>
2015-11-02 18:52:05 -0200

    biblioteca inicial

--- functions.c
+++ functions.c
@@ -4,3 +4,28 @@
  */
 
 
+
+
+void getchar(char *wordlist, int * lettercount){
+	
+	int pointerLen=20;
+	char letter;
+	while(1){
+                //realocando espaço
+                if(letterCount>=(pointerLen/2)){
+                       pointerLen*=2;
+                       wordList=(char *) realloc(wordList, pointerLen);
+                       
+                //pegando entrada do usuario
+                }else if(scanf("%c", &letter)!=EOF){
+                        
+                        wordList[letterCount]=letter;
+                        letterCount++;
+                //parando no EOF
+                }else{
+                        break;
+                }
+        }
+
+	return;
+}
