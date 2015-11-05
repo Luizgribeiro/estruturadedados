e56978bf105c155797f014033764ca02e53848cc
Luiz G. Ribeiro <ltrindaderibeiro@gmail.com>
2015-11-05 08:31:11 -0200

    add busca em lista sequencial

--- functions.c
+++ functions.c
@@ -4,22 +4,24 @@
  */
 
 
+#include <stdio.h>
+#include <stdlib.h>
 
-
-void getchar(char *wordlist, int * lettercount){
+//recebe char de forma dinamica
+void getword(char *wordList, int * letterCount){
 	
 	int pointerLen=20;
 	char letter;
 	while(1){
                 //realocando espaço
-                if(letterCount>=(pointerLen/2)){
+                if(*letterCount>=(pointerLen/2)){
                        pointerLen*=2;
                        wordList=(char *) realloc(wordList, pointerLen);
                        
                 //pegando entrada do usuario
                 }else if(scanf("%c", &letter)!=EOF){
                         
-                        wordList[letterCount]=letter;
+                        wordList[*letterCount]=letter;
                         letterCount++;
                 //parando no EOF
                 }else{
@@ -28,4 +30,17 @@ void getchar(char *wordlist, int * lettercount){
         }
 
 	return;
+}
+
+
+
+
+//busca em lista sequencial  --> substituir pelos tipos desejados
+int buscaSequencial(int * lista, int tamanho, int valor){
+        int i;
+        for(i=0;i<tamanho;i++){
+                if(*(lista+i)==valor){
+                        return i;
+                }
+        }
 }
